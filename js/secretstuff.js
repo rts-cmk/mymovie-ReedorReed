3d2f5706f2246e0ea1103c1dd5401d5fdf65a27b
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-09-16 08:50:36 +0200

    update

--- js/secretstuff.js
+++
@@ -1,7 +0,0 @@
-const apiCredits = {
-	headers: {
-		accept: 'application/json',
-		Authorization:
-			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGRhZmYzZmE3NWUyYmVkMjc5N2U5Mzc5NWUyMzNkMCIsIm5iZiI6MTc1NDU3MDQwMi44NDgsInN1YiI6IjY4OTQ5ZWEyNTgwOGQ2MTVlNDkyMjNlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.izL8ZRorjEdMvkctVUiMmYb0eu93iytVt1nvjRBZNfY'
-	}
-};
